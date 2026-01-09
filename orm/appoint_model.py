2395d34e8c8f7d71723b939d9bd24c9ff340579d
Fog <fogjuicexd@gmail.com>
2026-01-09 20:18:08 +0300

    database_init

--- orm/appoint_model.py
+++
@@ -1,8 +0,0 @@
-from tortoise.models import Model
-from tortoise import fields
-
-class Appoint(Model):
-    id = fields.IntField(pk= True)
-    user_id = fields.IntField()
-    master_id = fields.IntField()
-    date_time = fields.DateTimeField(null=False)
--- orm/master_model.py
+++
@@ -1,6 +0,0 @@
-from tortoise.models import Model
-from tortoise import fields
-
-class Master(Model):
-    id = fields.IntField(pk=True)
-    
