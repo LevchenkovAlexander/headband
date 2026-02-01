0fa20ba3cf612dfdb38541b6ae708c543427c00d
Grigorii Malyshev <grisha.malyshev.06@bk.ru>
2026-02-01 18:56:32 +0300

    add authtemplate and full logic registration except fetch to server

---
+++ sitepage/src/components/auth/login/login.tsx
@@ -0,0 +1,60 @@
+import "./login.scss";
+import { Link } from "react-router-dom";
+
+const Login = () => {
+  const handleSumbit = (e: React.SyntheticEvent) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="login">
+      <div className="login__title">Войти в аккаунт</div>
+      <div className="login__greeting">
+        Добро пожаловать в экосистему headband
+      </div>
+      <form className="login__form" onSubmit={handleSumbit}>
+        <label htmlFor="email"></label>
+        <input
+          id="email"
+          type="email"
+          name="email"
+          placeholder="Почта"
+          required
+        />
+
+        <label htmlFor="password"></label>
+        <input
+          id="password"
+          type="password"
+          name="password"
+          placeholder="Пароль"
+          required
+        />
+
+        <div className="login__controls">
+          <a href="#" className="login__forgot">
+            Забыли пароль?
+          </a>
+
+          <label className="login__remember">
+            <input type="checkbox" name="remember" />
+            <span>Запомнить меня</span>
+          </label>
+        </div>
+
+        <button className="login__submit" type="submit">
+          Войти
+        </button>
+      </form>
+      <button className="login__yandex" type="button">
+        Войти с Яндекс ID
+      </button>
+
+      <div className="login__bottom">
+        Нет аккаунта? <Link to="/registration">Создать</Link>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
