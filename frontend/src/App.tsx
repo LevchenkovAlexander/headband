42b50627e5babf7bbf402bbffb66fc04a6130dee
Grigorii Malyshev <grisha.malyshev.06@bk.ru>
2026-03-31 16:23:25 +0300

    main page complete

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.scss";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="app-wrapper">
+      <div className="mobile-version">
+        <div className="header">
+          <div className="header__greeting">good morning</div>
+          <div className="header__version">version for masters</div>
+        </div>
+
+        <div className="actual">
+          <div className="actual__header">
+            <div className="actual__title">Актуальное</div>
+            <span className="actual__underline"></span>
+          </div>
+          <div className="actual__wrapper">
+            <div className="actual__event">
+              <div className="actual__bar"></div>
+              <div className="actual__info">
+                <div className="actual__service">13:00 Покраска</div>
+                <div className="actual__subservice">14:30 Мелирование</div>
+              </div>
+            </div>
+            <div className="actual__rest">
+              <div className="actual__bar actual__bar--transparent"></div>
+              <div className="actual__break">Отдых 2 часа 20 минут</div>
+            </div>
+            
+            <div className="actual__event">
+              <div className="actual__bar"></div>
+              <div className="actual__info">
+                <div className="actual__service">13:00 Покраска</div>
+                <div className="actual__subservice">14:30 Мелирование</div>
+              </div>
+            </div>
+            <div className="actual__rest">
+              <div className="actual__bar actual__bar--transparent"></div>
+              <div className="actual__break">Отдых 2 часа 20 минут</div>
+            </div>
+          </div>
+        </div>
+
+        <div className="information">
+          <div className="information__header">
+            <div className="information__title">Информация</div>
+            <div className="information__underline"></div>
+          </div>
+          <div className="information__wrapper">
+            <div className="information__guides">Гайды</div>
+            <div className="information__schedule">Расписание</div>
+            <div className="information__profile">Профиль</div>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
